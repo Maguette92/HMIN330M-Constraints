971ae7afbd4131adc985ea9f3476c8576b5a33f3
Maguette92 <72444860+Maguette92@users.noreply.github.com>
2020-10-11 22:49:08 +0200

    Update GolombRuler.java

--- src/problems/GolombRuler.java
+++ src/problems/GolombRuler.java
@@ -14,6 +14,31 @@ public class GolombRuler extends AbstractProblem {
     public void buildModel() {
     	
     	// Build Model here. 
+        model = new Model();
+        
+        //Variables definition
+    	IntVar[] X = model.intVarArray("a", m, 0, m*m);
+    	//All distances
+    	IntVar[] Y = model.intVarArray("b", (m*(m-1))/2, 0, m*m);
+    	
+    	
+    	//Constraints definition
+                //for all i, Xi < Xi+1
+    	for(int i=0; i<=m-1; i++) {
+    		model.arithm(X[i], "<", X[i+1]).post();
+    	}
+    	        //force the first mark to take the value 0
+    	model.arithm(X[0], "=", 0).post();
+    	for (int i=0, j=0, k=0, l=0 ; i<=m-1 & j<=m-1 & k<=m-1 & l<=m-1 & i<j & k<j & ((j != l) || (i != k)) ; i++ , j++, k++, j++)
+    	{
+    		    //2 distinct marks are never at the same distance
+    		model.scalar(new IntVar[]{X[i],X[j],X[l],X[k]} , new int[]{1,-1,-1,1}, "!=", 0).post();
+    		model.arithm(Y[k], "=", X[i], "-", X[j]).post();
+    	}
+    	        // All distances have to be different
+    	model.allDifferent(Y).post();
+    	        //To break the symmetry
+    	model.scalar(new IntVar[]{X[1],X[0],X[m-1],X[m-2]} , new int[]{1,-1,-1,1}, "<", 0).post();
     }
 
     @Override
@@ -26,6 +51,7 @@ public class GolombRuler extends AbstractProblem {
     public void solve() {
   
     	// Set objective if needed;
+        model.getSolver().setObjective(Model.MINIMIZE, X[m-1]);
     	// Solve the instance
     	// Print the solution
         
